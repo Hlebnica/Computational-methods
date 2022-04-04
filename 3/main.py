fe9b5b8af8736766864607dc204cf49c09c9f69e
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-04-04 18:30:45 +0500

    88

--- 3/main.py
+++ 3/main.py
@@ -8,41 +8,57 @@ from sympy.abc import x, y
 # Используя метод простой итерации решить систему
 # уравнений с точностью до 0.001. Корни отделить графически. .subs(x, x_0)
 
-# y = cos(x-1) + 2y - 0.5
-# x = 2x - cos(y) - 3
-
 reduced_y = cos(x - 1) + y - 0.5
 reduced_x = x - cos(y) - 3
 
-x0 = 3.33
+x0 = 3.34
 y0 = 1.19
 
 
-def convergence_condition(x_0, y_0, reduce_y, reduce_x):  # Проверка сходимости
+def convergence_condition(x_0, y_0, func_y, func_x):  # Проверка сходимости
     x, y = symbols('x y')
-    partial_derivative_red_y_x = diff(reduce_y, x).subs(x, x_0)
-    partial_derivative_red_y_y = diff(reduce_y, y).subs(y, y_0)
+    partial_derivative_red_y_x = diff(func_y, x).subs(x, x_0)
+    partial_derivative_red_y_y = diff(func_y, y).subs(y, y_0)
 
-    partial_derivative_red_x_x = diff(reduce_x, x).subs(x, x_0)
-    partial_derivative_red_x_y = diff(reduce_x, y).subs(y, y_0)
+    partial_derivative_red_x_x = diff(func_x, x).subs(x, x_0)
+    partial_derivative_red_x_y = diff(func_x, y).subs(y, y_0)
 
     if max(partial_derivative_red_y_x, partial_derivative_red_y_y, partial_derivative_red_x_x,
            partial_derivative_red_x_y) <= 1:
         print("Метод сходится")
-        print(partial_derivative_red_y_x, "\n", partial_derivative_red_y_y, "\n", partial_derivative_red_x_x, "\n",
-              partial_derivative_red_x_y)
+        # print(partial_derivative_red_y_x, "\n", partial_derivative_red_y_y, "\n", partial_derivative_red_x_x, "\n",
+        #       partial_derivative_red_x_y)
     else:
         print("Метод не сходится")
-        print(partial_derivative_red_y_x, "\n", partial_derivative_red_y_y, "\n", partial_derivative_red_x_x, "\n",
-              partial_derivative_red_x_y)
+        # print(partial_derivative_red_y_x, "\n", partial_derivative_red_y_y, "\n", partial_derivative_red_x_x, "\n",
+        #       partial_derivative_red_x_y)
 
 
-def iterative_method(x_0, y_0, reduce_y, reduce_x):
-    exp = 0.0001
+def iterative_method(x_0, y_0, func_y, func_x):
+    exp = 0.001
     x_n = x_0
     y_n = y_0
+    max_iteration = 100
+    k = 0
+    while k <= max_iteration:
+        k += 1
+        x_n1 = func_x.subs([(x, x_n), (y, y_n)])
+        y_n1 = func_y.subs([(x, x_n), (y, y_n)])
 
-    pass
+        print("x_n1", x_n1)
+        print("x_n", x_n)
+
+        print("y_n1", y_n1)
+        print("y_n", y_n)
+
+        if abs(x_n1 - x_n) <= exp and abs(y_n1 - y_n) <= exp:
+            break
+        x_n = x_n1
+        y_n = y_n1
+
+    return x_n, y_n
 
 
 convergence_condition(x0, y0, reduced_y, reduced_x)
+
+print(iterative_method(x0, y0, reduced_y, reduced_x))
