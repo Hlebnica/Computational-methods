fa8ca4d1cf22dabf429e2c8a8a1208678e59833c
Hlebnica <49001181+Hlebnica@users.noreply.github.com>
2022-04-09 01:01:39 +0500

    555

--- 5/main.py
+++ 5/main.py
@@ -1,4 +1,6 @@
 from scipy import integrate
+from sympy import diff, sqrt
+from sympy.abc import x
 
 
 #  Формула прямоугольников
@@ -13,6 +15,21 @@ def func(x):
     return (x ** 2) / 2
 
 
-integral = integrate.quad(func, -1, 1)
+def printer(upper_limit_, lower_limit_, function):
+    print(upper_limit_)
+    print('|')
+    print('|', f'{function}dx = ', integral[0])
+    print('|')
+    print(lower_limit_)
 
-print('', integral[0])
+
+def m_2():
+    return True
+
+
+lower_limit = -1
+upper_limit = 1
+
+integral = integrate.quad(func, lower_limit, upper_limit)
+
+printer(upper_limit, lower_limit, "(x^2/2)")
